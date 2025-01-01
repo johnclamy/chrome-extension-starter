5721a6e42eb4c100ae6cc479d111456c67e95eee
John_Lamy <johnclamy@gmail.com>
2025-01-01 09:16:45 +0000

    create a movie model

--- moviestore/movies/models.py
+++ moviestore/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+  id = models.AutoField(primary_key=True)
+  title = models.CharField(max_length=255)
+  price = models.IntegerField()
+  descr = models.TextField()
+  image = models.ImageField(upload_to='movie_imgs/')
+
+  def __str__(self) -> str:
+    return str(self.id) + ' - ' + self.title
