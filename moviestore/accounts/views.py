d2bbc332785ad87786ad4db57f642d46be42662a
John_Lamy <johnclamy@gmail.com>
2025-01-07 13:21:40 +0000

    define the signin function in '/accounts/views.py

--- moviestore/accounts/views.py
+++ moviestore/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, authenticate
 from . forms import CustomUserCreationForm, CustomErrorList
 
 
@@ -19,3 +20,26 @@ def signup(request):
     else:
       template_data['form'] = form
       return render(request, 'accounts/signup.html', {'template_data': template_data})
+
+
+def signin(request):
+  template_data = {}
+  template_data['title'] = 'Signin'
+
+  if request.method == 'GET':
+    return render(request, 'accounts/signin.html', {'template_data': template_data})
+  elif request.method == 'POST':
+    user = authenticate(
+      request,
+      username = request.POST['username'],
+      password = request.POST['password']
+    )
+
+    if user is None:
+      template_data['error'] = 'Username or password is incorrect.'
+      return render(request, 'accounts/signin.html', {'template_data': template_data})
+    else:
+      auth_login(request, user)
+      return redirect('home.index')
+    
+
