0abc2dedeef2f4e3cf12691c322a0647f6dd1bc2
John_Lamy <johnclamy@gmail.com>
2025-01-07 14:16:56 +0000

    configure the sign out URL

--- moviestore/accounts/urls.py
+++ moviestore/accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
   path('signup', views.signup, name='accounts.signup'),
   path('signin', views.signin, name='accounts.signin'),
+  path('sign-out/', views.sign_out, name='accounts.sign-out'),
 ]
