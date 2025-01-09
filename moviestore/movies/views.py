42d3f6f5a5bbb5ca7268edaf11f7ce7046b7bd38
John_Lamy <johnclamy@gmail.com>
2025-01-09 19:56:03 +0000

    define a edit_review func to collect the reviewed data and pass it to the editing template

--- moviestore/movies/views.py
+++ moviestore/movies/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from . models import Movie,Review
+from . models import Movie, Review
 
 
 def index(request):
@@ -39,6 +39,28 @@ def create_review(request, id):
     review.user = request.user
     review.save()
 
+    return redirect('movies.show', id=id)
+  else:
+    return redirect('movies.show', id=id)
+  
+
+@login_required
+def edit_review(request, id, review_id):
+  review = get_object_or_404(Review, id=review_id)
+
+  if request.user != review.user:
+    return redirect('movies.show', id=id)
+
+  if request.method == 'GET':
+    template_data = {}
+    template_data['title'] = 'Edit Review'
+    template_data['review'] = review
+    return render(request, 'movies/edit_review.html', {'template_data': template_data})
+  
+  elif request.method == 'POST' and request.POST['comment'] != '':
+    review = Review.objects.get(id=review_id)
+    review.comment = request.POST['comment']
+    review.save()
     return redirect('movies.show', id=id)
   else:
     return redirect('movies.show', id=id)
